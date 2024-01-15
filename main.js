fcbad34e58d5a27795049d69d86e2669dab28f09
lroux07 <lroux@edenschool.fr>
2024-01-15 13:32:43 +0100

    finish day 1

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-let cards = document.querySelectorAll(".cards")
+let cards = document.querySelectorAll(".cards");
+let returnCard1 = "";
+let returnCard2 = "";
 
 function clickCards() {
   for(let i = 0; i < cards.length; i++) {
     cards[i].addEventListener("click", ()=> {
-      cards[i].style.backgroundColor = "rgb(228, 228, 228)";
-      cards[i].style.color = "#1e1e1e";
-    })
+      if(returnCard1 == "") {
+        returnCard1 = cards[i].textContent;
+        cards[i].style.backgroundColor = "rgb(228, 228, 228)";
+        cards[i].style.color = "#1e1e1e";
+      } else if(returnCard2 == "") {
+        returnCard2 = cards[i].textContent;
+        cards[i].style.backgroundColor = "rgb(228, 228, 228)";
+        cards[i].style.color = "#1e1e1e";
+      } else if(returnCard1 !== returnCard2 ) {
+        cards[i].style.backgroundColor = "violet";
+        cards[i].style.color = "violet";
+      }
+    });
   }
 }
 
